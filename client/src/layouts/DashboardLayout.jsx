4d801c383a28b0460a48b986a487c6b9b395a097
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-08-31 18:43:58 +0530

    updation in the code

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import { NavLink, Outlet } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext.jsx';
+// import { useAuth } from '../context/AuthContext.jsx';
 import Header from '../components/layout/Header.jsx';
 
 export default function DashboardLayout({ sidebarLinks }) {
