10ab2a15d037aa60459716598c267b8f4c3505c3
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-16 22:36:20 +0530

    commit

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -4,7 +4,7 @@ import { z } from 'zod';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import { useAuth } from '../../context/AuthContext.jsx';
-import { t } from '../../i18n/index.js';
+// import { t } from '../../i18n/index.js';
 // import Input from '../../components/ui/Input.jsx';
 // import Button from '../../components/ui/Button.jsx';
 // import Card from '../../components/ui/Card.jsx';
