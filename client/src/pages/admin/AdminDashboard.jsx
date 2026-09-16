0244c14c4eb3f41ad8d4c43bedfc500a94c2231d
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-16 21:33:20 +0530

    commit

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -64,7 +64,7 @@ export default function AdminDashboard() {
               <li key={i} className="flex items-center justify-between text-sm">
                 <div>
                   {/* <p className="font-medium text-gray-900">{a.action}</p> */}
-                  <p className="text-gray-500">{a.user}</p>
+                  {/* <p className="text-gray-500">{a.user}</p> */}
                 </div>
                 {/* <span className="text-xs text-gray-400">{a.time}</span> */}
               </li>
