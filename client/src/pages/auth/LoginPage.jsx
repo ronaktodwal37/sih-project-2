9f6f85bf9b8319d6d07263f46f3f1559c0184770
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-16 22:41:06 +0530

    commit

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -1,5 +1,5 @@
 // import { useForm } from 'react-hook-form';
-// import { zodResolver } from '@hookform/resolvers/zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useState } from 'react';
