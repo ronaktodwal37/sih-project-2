efc60c94c5d02113085da4cbf5f5bd617efc62af
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-16 22:40:16 +0530

    commit

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -11,7 +11,7 @@
 
 const schema = z.object({
   email: z.string().email('Invalid email address'),
-//   password: z.string().min(1, 'Password is required'),
+  password: z.string().min(1, 'Password is required'),
 });
 
 export default function LoginPage() {
