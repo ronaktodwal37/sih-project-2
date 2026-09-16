a453c027d5a6b6f761c52cb51758462410e1c5c4
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-16 22:47:37 +0530

    commit

--- client/src/pages/auth/RegisterPage.jsx
+++ client/src/pages/auth/RegisterPage.jsx
@@ -4,7 +4,7 @@ import { z } from 'zod';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { useAuth } from '../../context/AuthContext.jsx';
-// import { t } from '../../i18n/index.js';
+import { t } from '../../i18n/index.js';
 // import { ROLES } from '../../utils/constants.js';
 // import Input from '../../components/ui/Input.jsx';
 // import Select from '../../components/ui/Select.jsx';
